815eadb9a84e879c46d12c42f5879b4e1a31bcfd
TJ-Xu <104058112+TJ-Xu@users.noreply.github.com>
2025-02-27 12:45:16 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------
-# TJ Xu et al. Large language model revealing hidden biosynthetic potential of the global ocean microbiome.
+# TJ Xu et al. DeepSeMS: a large language model reveals hidden biosynthetic potential of the global ocean microbiome.
 # ----------------------------------------------------------------
 # References: 
 # https://github.com/huggingface/transformers; 
